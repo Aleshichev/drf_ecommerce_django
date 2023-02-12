9d0ec2c700c8fb154f2bfc075d05e1483cbf4142
Aloshychev Ihor <hiranyagarbha@i.ua>
2023-02-12 08:22:08 +0200

    Test_1

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,18 +1,46 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.response import Response
-from drf_spectacular.utils import extend_schema
-from .models import Category
-from .seriallizers import CategorySerializer
+
+from .models import Brand, Category, Product
+from .seriallizers import BrandSerializer, CategorySerializer, ProductSerializer
 
 
 class CategoryViewSet(viewsets.ViewSet):
     """
-    A simple Viewset for viewing al categories
+    A simple Viewset for viewing all categories
     """
+
     queryset = Category.objects.all()
 
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all Brands
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all Products
+    """
+
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
